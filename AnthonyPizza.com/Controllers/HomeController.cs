73a669c1a8810deda425ab35a645fd2f227a1df5
Ritesh Sinha <Ritesh.Sinha@mLogica.com>
2023-07-26 21:11:14 +0100

    Controller is added !

---
+++ AnthonyPizza.com/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AnthonyPizza.com.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult AboutUs()
+        {
+            return View();
+
+        }
+        public ActionResult menu()
+        {
+            return View();
+        }
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+    }
+}
